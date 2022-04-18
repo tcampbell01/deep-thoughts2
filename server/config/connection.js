0a787c1b846d8a094be932faeff2a4c2cc5898fc
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-04-18 17:37:17 -0400

    initial files

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/deep-thoughts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  
+  
+});
+
+module.exports = mongoose.connection;
